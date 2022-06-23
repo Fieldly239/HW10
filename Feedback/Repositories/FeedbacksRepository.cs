71f8129874faa3658c0cde06bfc0221df821b1e7
Phatsakorn Rodphol (NTL) <CF303957@tidlor.com>
2022-06-23 15:05:07 +0700

    init my app

---
+++ Feedback/Repositories/FeedbacksRepository.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using Feedback.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Repositories
+{
+    public class FeedbacksRepository : IFeedbacksRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly FeedbackMgmtContext _feedbackMgmtContext;
+
+        public FeedbacksRepository(FeedbackMgmtContext feedbackMgmtContext, IConfiguration configuration)
+        {
+            _feedbackMgmtContext = feedbackMgmtContext;
+            _configuration = configuration;
+        }
+        public IEnumerable<Feedbacks> GetAllFeedbacks()
+        {
+            using (var db = new SqlConnection(_configuration.GetSection("ConnectionStrings:ConnectionString").Value))
+            {
+                var sqlCommand = string.Format(@"SELECT * FROM [Feedbacks]");
+                return db.Query<Feedbacks>(sqlCommand).ToList();
+            }
+            
+        }
+        public Feedbacks GetByIdFeedbacks(int id)
+        {
+            using (var db = new SqlConnection(_configuration.GetSection("ConnectionStrings:ConnectionString").Value))
+            {
+                var sqlCommand = string.Format(@"SELECT * FROM [Feedbacks] WHERE Id = @Id");
+                return db.Query<Feedbacks>(sqlCommand, new { Id = id}).FirstOrDefault();
+            }
+        }
+
+        public int AddFeedbacks(Feedbacks feedbacks)
+        {
+            using (var db = new SqlConnection(_configuration.GetSection("ConnectionStrings:ConnectionString").Value))
+            {
+                var sqlCommand = string.Format(@"INSERT INTO [Feedbacks]
+                                               ([FeedbackName]
+                                               ,[Description]
+                                               ,[ApplicationId])
+                                         VALUES
+                                               (@FeedbackName
+                                               ,@Description
+                                               ,@ApplicationId)");
+                return db.Execute(sqlCommand, ParameterMapping(feedbacks));
+            }
+        }
+        public int UpdateFeedbacks(Feedbacks feedbacks)
+        {
+            using (var db = new SqlConnection(_configuration.GetSection("ConnectionStrings:ConnectionString").Value))
+            {
+                var sqlCommand = string.Format(@"UPDATE [Feedbacks]
+                                               SET [FeedbackName] = @FeedbackName
+                                                  ,[Description] = @Description
+                                                  ,[ApplicationId] = @ApplicationId
+                                             WHERE [Id] = @Id");
+                return db.Execute(sqlCommand, ParameterMapping(feedbacks));
+            }
+        }
+        public int DeleteFeedbacks(int id)
+        {
+            using (var db = new SqlConnection(_configuration.GetSection("ConnectionStrings:ConnectionString").Value))
+            {
+                var sqlCommand = string.Format(@"DELETE FROM [Feedbacks] WHERE [Id] = @Id");
+                return db.Execute(sqlCommand, new { Id = id});
+            }
+        }
+
+        private Object ParameterMapping(Feedbacks feedbacks)
+        {
+            return new
+            {
+                Id = feedbacks.Id,
+                FeedbackName = feedbacks.FeedbackName,
+                Description = feedbacks.Description,
+                ApplicationId = feedbacks.ApplicationId
+            };
+        }
+
+        
+
+        
+    }
+}
---
+++ Feedback/Repositories/IFeedbacksRepository.cs
@@ -0,0 +1,13 @@
+﻿using Feedback.Models;
+
+namespace Feedback.Repositories
+{
+    public interface IFeedbacksRepository
+    {
+        IEnumerable<Feedbacks> GetAllFeedbacks();
+        Feedbacks GetByIdFeedbacks(int id);
+        int AddFeedbacks(Feedbacks feedbacks);
+        int UpdateFeedbacks(Feedbacks feedbacks);
+        int DeleteFeedbacks(int id);
+    }
+}
